e899757e372eb510dbfa06cfe87dd919da4bcfb8
Viorel Lazari <vlazari@gmail.com>
2023-03-22 15:51:53 +0100

    bag fix

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,8 +7,8 @@ export default function Header() {
 	return (
 		<div className="sticky top-0 z-20 bg-slate-900 text-white opacity-95">
 			<div className="max-w-7xl mx-auto flex px-10 justify-between items-center">
-				<Nav />
 				<img className="h-16" src={Logo} alt="Logo" />
+				<Nav />
 				<CartIcon />
 			</div>
 		</div>
